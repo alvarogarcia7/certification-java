4dccb906b25c786bbe87d238df86acf515c378b5
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2015-10-28 23:19:42 +0100

    add third break test

--- 1Z0-808/src/test/exam/BreakTest.java
+++ 1Z0-808/src/test/exam/BreakTest.java
@@ -31,4 +31,21 @@ public class BreakTest {
         }
         System.out.println("4");
     }
+    
+  @Test public void break_3() {
+      System.out.println("0");
+      PARENT_LOOP:   if (true){
+          System.out.println("1");
+          INNER_LOOP:if(true) {
+              for(int i=0;i<5;i++) {
+                  System.out.println("2");
+                  break PARENT_LOOP;
+              }
+              System.out.println("2B");
+              break PARENT_LOOP;
+          }
+          System.out.println("3");
+      }
+      System.out.println("4");
+    }
 }
