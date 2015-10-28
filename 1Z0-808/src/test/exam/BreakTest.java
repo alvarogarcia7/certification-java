2de2920dfbc67e345f9d9826d22379228b023c7f
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2015-10-28 23:20:05 +0100

    whitespace

--- 1Z0-808/src/test/exam/BreakTest.java
+++ 1Z0-808/src/test/exam/BreakTest.java
@@ -16,22 +16,22 @@ public class BreakTest {
   }
   
   @Test public void break_2() {
-        System.out.println("0");
-        PARENT_LOOP:   if (true){
-            System.out.println("1");
-            INNER_LOOP:if(true) {
-                for(int i=0;i<5;i++) {
-                    System.out.println("2");
-                    break INNER_LOOP;
-                }
-                System.out.println("2B");
-                break PARENT_LOOP;
+    System.out.println("0");
+    PARENT_LOOP:   if (true){
+        System.out.println("1");
+        INNER_LOOP:if(true) {
+            for(int i=0;i<5;i++) {
+                System.out.println("2");
+                break INNER_LOOP;
             }
-            System.out.println("3");
+            System.out.println("2B");
+            break PARENT_LOOP;
         }
-        System.out.println("4");
+        System.out.println("3");
     }
-    
+    System.out.println("4");
+  }
+  
   @Test public void break_3() {
       System.out.println("0");
       PARENT_LOOP:   if (true){
